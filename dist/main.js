4979b212b625d7b32f79ca25f6a157b8b42c761f
Minha <minha.ahmad5982@gmail.com>
2023-11-13 08:25:52 +0100

    hey

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,27 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/scripts/headline.js
+/* harmony default export */ const headline = ((function headline() {
+	const headline = "Welcome to the webpage"
+	document.querySelector("h1").innerText = headline
+})());
+
+;// CONCATENATED MODULE: ./src/scripts/burgermenu.js
+/* harmony default export */ const burgermenu = ((function burgermenu() {
+	const button = document.querySelector(".menuButton")
+	const menu = document.querySelector(".primaryMenu")
+
+	button.addEventListener("click", clickHandler)
+
+	function clickHandler() {
+		menu.style.right = menu.style.right == "-19em" ? "0" : "-19em"
+	}
+})());
+;// CONCATENATED MODULE: ./src/scripts/api.js
 const URL = new URLSearchParams(window.location.search)
-export default (async function () {
+/* harmony default export */ const api = ((async function () {
     const NEWS = document.querySelector(".nyhed")
     //her får jeg fat i min div i min html 
     await fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=F7Q16NruiZVUiGVH934NpdQgCBkkIz2s')
@@ -23,4 +45,12 @@ export default (async function () {
             });
             console.log(data);
         });
-})()
+})());
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
